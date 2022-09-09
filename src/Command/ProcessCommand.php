f73d4b0c4b410767acaf42e8d47f3ce2ad128ed5
Joachim Løvgaard <joachim@loevgaard.dk>
2022-09-09 13:57:09 +0200

    Add logging to notification dispatcher

--- src/Command/ProcessCommand.php
+++ src/Command/ProcessCommand.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Setono\SyliusAbandonedCartPlugin\Command;
 
+use Psr\Log\LoggerAwareInterface;
 use Setono\SyliusAbandonedCartPlugin\Dispatcher\NotificationDispatcherInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 
 final class ProcessCommand extends Command
@@ -24,6 +26,10 @@ final class ProcessCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        if ($this->notificationDispatcher instanceof LoggerAwareInterface && $output->isDebug()) {
+            $this->notificationDispatcher->setLogger(new ConsoleLogger($output));
+        }
+
         $this->notificationDispatcher->dispatch();
 
         return 0;
--- src/Dispatcher/NotificationDispatcher.php
+++ src/Dispatcher/NotificationDispatcher.php
@@ -6,6 +6,9 @@ namespace Setono\SyliusAbandonedCartPlugin\Dispatcher;
 
 use DateInterval;
 use Doctrine\Persistence\ManagerRegistry;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerInterface;
+use Psr\Log\NullLogger;
 use Setono\DoctrineObjectManagerTrait\ORM\ORMManagerTrait;
 use Setono\SyliusAbandonedCartPlugin\Message\Command\ProcessNotification;
 use Setono\SyliusAbandonedCartPlugin\Model\NotificationInterface;
@@ -15,9 +18,11 @@ use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\Registry;
 use Symfony\Component\Workflow\WorkflowInterface;
 
-final class NotificationDispatcher implements NotificationDispatcherInterface
+final class NotificationDispatcher implements NotificationDispatcherInterface, LoggerAwareInterface
 {
     use ORMManagerTrait;
+
+    private LoggerInterface $logger;
 
     private ?WorkflowInterface $workflow = null;
 
@@ -37,6 +42,7 @@ final class NotificationDispatcher implements NotificationDispatcherInterface
         int $idleThresholdInMinutes
     ) {
         $this->managerRegistry = $managerRegistry;
+        $this->logger = new NullLogger();
         $this->commandBus = $commandBus;
         $this->notificationRepository = $notificationRepository;
         $this->workflowRegistry = $workflowRegistry;
@@ -49,9 +55,22 @@ final class NotificationDispatcher implements NotificationDispatcherInterface
             new DateInterval(sprintf('PT%dM', $this->idleThresholdInMinutes))
         );
 
+        $this->logger->debug(sprintf(
+            'Notifications with associated orders that have not been updated for %d minutes will be notified',
+            $this->idleThresholdInMinutes
+        ));
+
+        $dispatchCount = 0;
+
         foreach ($notifications as $notification) {
             $workflow = $this->getWorkflow($notification);
             if (!$workflow->can($notification, NotificationWorkflow::TRANSITION_START)) {
+                $this->logger->warning(sprintf(
+                    'The notification with id %d could not take the transition "%s"',
+                    (int) $notification->getId(),
+                    NotificationWorkflow::TRANSITION_START
+                ));
+
                 continue;
             }
 
@@ -60,7 +79,16 @@ final class NotificationDispatcher implements NotificationDispatcherInterface
             $this->getManager($notification)->flush();
 
             $this->commandBus->dispatch(new ProcessNotification($notification));
+
+            ++$dispatchCount;
         }
+
+        $this->logger->debug(sprintf('%d notifications dispatched for processing', $dispatchCount));
+    }
+
+    public function setLogger(LoggerInterface $logger): void
+    {
+        $this->logger = $logger;
     }
 
     private function getWorkflow(NotificationInterface $notification): WorkflowInterface
