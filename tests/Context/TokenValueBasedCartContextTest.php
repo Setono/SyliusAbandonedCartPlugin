81f3c1701280f607a3d1f50eaac3ebadb3904a52
Joachim Løvgaard <joachim@loevgaard.dk>
2022-09-20 10:09:58 +0200

    Add test for TokenValueBasedCartContext

---
+++ tests/Context/TokenValueBasedCartContextTest.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Setono\SyliusAbandonedCartPlugin\Context;
+
+use PHPUnit\Framework\TestCase;
+use Prophecy\PhpUnit\ProphecyTrait;
+use Setono\SyliusAbandonedCartPlugin\Context\TokenValueBasedCartContext;
+use Sylius\Component\Core\Model\Order;
+use Sylius\Component\Core\Repository\OrderRepositoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * @covers \Setono\SyliusAbandonedCartPlugin\Context\TokenValueBasedCartContext
+ */
+final class TokenValueBasedCartContextTest extends TestCase
+{
+    use ProphecyTrait;
+
+    /**
+     * @test
+     */
+    public function it_returns_cart(): void
+    {
+        $requestStack = new RequestStack();
+        $requestStack->push(new Request([
+            'tokenValue' => 'token',
+        ]));
+
+        $order = new Order();
+
+        $orderRepository = $this->prophesize(OrderRepositoryInterface::class);
+        $orderRepository->findCartByTokenValue('token')->willReturn($order);
+        $context = new TokenValueBasedCartContext($requestStack, $orderRepository->reveal());
+
+        self::assertSame($order, $context->getCart());
+    }
+}
