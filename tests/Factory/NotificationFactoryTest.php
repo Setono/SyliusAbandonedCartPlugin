03c27dac778a7b5ea1a410874b7395843b3f0a5e
Joachim Løvgaard <joachim@loevgaard.dk>
2022-09-20 10:03:57 +0200

    Add test for NotificationFactory

---
+++ tests/Factory/NotificationFactoryTest.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Setono\SyliusAbandonedCartPlugin\Factory;
+
+use PHPUnit\Framework\TestCase;
+use Setono\SyliusAbandonedCartPlugin\Factory\NotificationFactory;
+use Setono\SyliusAbandonedCartPlugin\Model\Notification;
+use Sylius\Component\Core\Model\Order;
+use Sylius\Component\Resource\Factory\Factory;
+
+/**
+ * @covers \Setono\SyliusAbandonedCartPlugin\Factory\NotificationFactory
+ */
+final class NotificationFactoryTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_creates_with_cart(): void
+    {
+        $order = new Order();
+
+        $factory = new NotificationFactory(new Factory(Notification::class));
+        $notification = $factory->createWithCart($order);
+
+        self::assertSame($order, $notification->getCart());
+    }
+}
