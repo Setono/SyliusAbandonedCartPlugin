976302a1502f42c01ce76885b207dfd403989e10
Joachim Løvgaard <joachim@loevgaard.dk>
2024-10-08 09:49:17 +0200

    Fix dep issues

--- composer-dependency-analyser.php
+++ composer-dependency-analyser.php
@@ -1,7 +1,9 @@
 <?php
 
 use ShipMonk\ComposerDependencyAnalyser\Config\Configuration;
+use ShipMonk\ComposerDependencyAnalyser\Config\ErrorType;
 
 return (new Configuration())
     ->addPathToExclude(__DIR__ . '/tests')
+    ->ignoreErrorsOnPackage('symfony/validator', [ErrorType::UNUSED_DEPENDENCY])
 ;
